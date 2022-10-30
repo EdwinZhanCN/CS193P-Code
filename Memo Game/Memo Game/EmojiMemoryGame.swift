09a468332e5271b3da959c2bf8a44adbc21b3727
EdwinZhan <ed_zhan@outlook.com>
2022-10-30 11:22:58 +0800

    update

--- Memo Game/Memo Game/EmojiMemoryGame.swift
+++ Memo Game/Memo Game/EmojiMemoryGame.swift
@@ -11,7 +11,7 @@ class EmojiMemoryGame: ObservableObject {
     @Published private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
     
     private static func createMemoryGame() -> MemoryGame<String>{
-        let emojis = ["👻", "🕷", "🎃"]
+        let emojis = ["👻", "🕷", "🎃", "😧"]
         return MemoryGame<String>(numberOfPairsOfCards: emojis.count) { pairIndex in
             return emojis[pairIndex]
         }
